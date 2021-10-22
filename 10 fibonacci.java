d5c39a018152b33c5a3444e99aed9ce1fb1628ec
Muskan Malhotra <muskan01malhotra@gmail.com>
2021-10-22 22:39:48 +0530

    Completed Fibonacci with Recursion

--- 10 fibonacci.java
+++ 10 fibonacci.java
@@ -5,10 +5,20 @@ class fibonacci{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
 
-    fibo(n);
+    int ans = fibo(n);
+    System.out.println(ans);
   }
 
-  public static void fibo(int n){
-    
+  public static int fibo(int n){
+
+    if(n == 0){
+      return 0;
+    }
+    if(n == 1){
+      return 1;
+    }
+    int ans = fibo(n-1) + fibo(n-2);
+
+    return ans;
   }
 }
