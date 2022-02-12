a0e8e8a9b60ab516aa8395974d6fb4c4009b4c63
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-02-12 13:07:14 +0530

    Linear Check

--- Recursion_With_Arrays/09 LinearCheck1.java
+++ Recursion_With_Arrays/09 LinearCheck1.java
@@ -1,14 +1,23 @@
 package Recursion_With_Arrays;
+/**
+ * Question: Check if a straight line can be drawn from the given X and Y coordinates.
+ * Brute force: Using for loop to check if the x coordinates for all the points are same ans vice versa.
+ */
+import java.util.*;
 
+class LinearizeCheck1 {
+  public static void main(String[] args){
+    //taking user input in pair
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] X = new int[n];
+    int[] Y = new int[n];
 
+    for(int i=0;i<n;i++){
+      X[i] = scn.nextInt();
+      Y[i] = scn.nextInt();
 
-import java.io.*;
-
-class LinearizeCheck {
-  public static void main(String[] args) throws IOException{
-    int[] X = {4,4,3,4,4};
-    int[] Y = {0,1,2,0,0};
-
+    }
     int ans = Linear(X,Y,0);
 
     if(ans == 0){
@@ -33,10 +42,13 @@ class LinearizeCheck {
       return 3;
     }
     
-    i++;
-    
-    int state = Linear(X,Y,i);
+    //look at the function call different in both the programs
+
+    int state = Linear(X,Y,i+1);
     // System.out.println(i);
+    // here i is behind the actual index i.t i here will stop on 3. for n at 5
+
+    // state = 0 (VX, HX); state = 1 (V); state = 2 (H); state = 3 (HV)
     if(state == 0){
       return 0;
     }
@@ -49,7 +61,7 @@ class LinearizeCheck {
       }
     }
     else if(state == 2){
-      if(Y[i] == Y[i-1]){
+      if(Y[i] == Y[i+1]){
         return 2;
       }
       else{
@@ -57,7 +69,7 @@ class LinearizeCheck {
       }
     }
     else if(state == 3){
-      if(X[i] == X[i-1] && Y[i-1] == Y[i]){
+      if(X[i] == X[i-1] && Y[i+1] == Y[i]){
         return 3;
       }
       else if(Y[i] == Y[i-1]){
