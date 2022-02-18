b859a54d1a84ff4e3566370533fc949a8ad21d0b
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-02-18 18:19:50 +0530

    Get the Stair Path

---
+++ Recursion_With_ArrayList/04 GetStairPath.java
@@ -0,0 +1,49 @@
+package Recursion_With_ArrayList;
+
+import java.util.*;
+import java.io.*;
+
+class GetStairPath{
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+
+    ArrayList<String> ans = gsp(n);
+    System.out.println(ans);
+  }
+
+  public static ArrayList<String> gsp(int n){
+
+    if(n == 0){
+      ArrayList<String> base = new ArrayList<>();
+      base.add("");
+      return base;
+    }
+
+    if(n<0){
+      ArrayList<String> base = new ArrayList<>();
+      return base;
+    }
+
+    ArrayList<String> nm1 = gsp(n-1);  //n-1 to 0   3 to 0   After jump of 1 standing on 3
+    ArrayList<String> nm2 = gsp(n-2);  //n-2 to 0   2 to 0   After jump of 2 standing on 2
+    ArrayList<String> nm3 = gsp(n-3);  //n-3 to 0   1 to 0   After jump of 3 standing on 1
+
+    ArrayList<String> myans = new ArrayList<>();
+
+    for(int i=0;i<nm1.size();i++){
+      //now n to 0;  4 to 3 + 3 to 0
+      myans.add("1"+nm1.get(i));
+    }
+    for(int i=0;i<nm2.size();i++){
+      //now n to 0;  4 to 3 + 3 to 0
+      myans.add("2"+nm2.get(i));
+    }
+    for(int i=0;i<nm3.size();i++){
+      //now n to 0;  4 to 3 + 3 to 0
+      myans.add("3"+nm3.get(i));
+    }
+
+    return myans;
+  }
+}
