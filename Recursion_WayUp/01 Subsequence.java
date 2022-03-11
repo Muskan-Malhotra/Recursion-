1b47f6fa8f6df5b19fc0df861f18733c67882d06
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-03-11 20:48:09 +0530

    Subsequence

--- Recursion_WayUp/01 Subsequence.java
+++ Recursion_WayUp/01 Subsequence.java
@@ -1,7 +1,45 @@
 package Recursion_WayUp;
+/**
+ * ALGORITHM
+ * levels and options are important 
+ * levels of a,b,c and options of yes or no.
+ * So 2^3 is the time complexity 2 as the options and 3 being the levels.
+ * psf = Path So Far
+ * 
+ */
+
+
+import java.io.*;
 
 class Subsequence {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException{
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+
+    Subseq(str,"");
+  }
+
+  public static void Subseq(String str, String psf){
+
+    if(str.length()==0){
+      System.out.print(psf+" ");
+      return;
+    }
+
+    // ? IMPORTANT: after adding 'a' as str(0) the psf(path so far) next time the str = remstr and b as str(0)
+
+    // for(int i=0;i<str.length();i++){  do't use not required print duplicate values
+      char ch = str.charAt(0);  //a
+      String remstr = str.substring(1);  //bc
+
+      //Yes choice
+      Subseq(remstr,psf+ch);
+
+      //No choice
+      Subseq(remstr,psf);
+
+    // }
     
   }
 }
--- Recursion_WayUp/02 Factorial.java
+++ Recursion_WayUp/02 Factorial.java
@@ -1,17 +1,24 @@
 package Recursion_WayUp;
 
+import java.util.*;
+
 class Factorial {
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int n = scn.nextInt();
 
     fact(n,1);
+    
+    scn.close();
   }
 
   public static void fact(int n,int ans){
     if(n == 0){
       System.out.println(ans);
+      return;
     }
+    // System.out.println(n);
     fact(n-1,ans*n);
   }
+  
 }
