eb0e03092a0a42a86bf1e7812dd2debac9d27f9c
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-03-12 13:45:22 +0530

    Print Stair Path

---
+++ Recursion_WayUp/04 PrintStrairPath.java
@@ -0,0 +1,28 @@
+package Recursion_WayUp;
+
+import java.util.*;
+
+class PrintStrairPath {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    prSP(n,"");
+  }
+
+  public static void prSP(int n, String path) {
+        
+    if(n==0){
+        System.out.println(path);
+        return;
+    }
+    if(n<0){
+        return;
+    }
+    
+    prSP(n-1,path+'1');
+    prSP(n-2,path+'2');
+    prSP(n-3,path+'3');
+}
+  
+}
