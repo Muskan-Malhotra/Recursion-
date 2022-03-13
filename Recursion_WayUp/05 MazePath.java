d9bd0822034bedbc98c1eb30f6e414efc41e2363
Muskan Malhotra <muskan01malhotra@gmail.com>
2022-03-13 13:14:53 +0530

    Maze Path Done

--- Recursion_WayUp/05 MazePath.java
+++ Recursion_WayUp/05 MazePath.java
@@ -1,5 +1,36 @@
 package Recursion_WayUp;
 
+import java.util.*;
+
 class MazePath {
-  
+  public static void main(){
+    Scanner scn = new Scanner(System.in);
+    int dr = scn.nextInt();
+    int dc = scn.nextInt();
+    
+    printMazePaths(0,0,dr-1,dc-1,"");
+
+    scn.close();
+  }
+
+  // sr - source row
+	    // sc - source column
+	    // dr - destination row
+	    // dc - destination column
+	    public static void printMazePaths(int sr, int sc, int dr, int dc, String psf) {
+        if(sr == dr && sc == dc){
+            System.out.println(psf);
+            return;
+        }
+        if(sr > dr){
+            return;
+       }
+       if(sc > dc){
+           return;
+       }
+        
+        printMazePaths(sr,sc+1,dr,dc,psf+"h");
+        printMazePaths(sr+1,sc,dr,dc,psf+"v");
+        
+    }
 }
